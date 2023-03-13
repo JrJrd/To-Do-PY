ba50b1042807d831156790fb89f059a7cc25459a
atSeaer <jrd.seaer@gmail.com>
2023-03-13 03:45:24 -0700

    if-in and slicing
    As I needed the user to add input to the list without selecting the adding function first, I replaced the match-case with if-conditionals.
    
    Slicing was required to remove the input "add" from the user_action.

--- py.py
+++ py.py
@@ -1,45 +1,58 @@
-
 while True:
     user_action = input(" type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
-    match user_action:
-        case 'add':
-            todo = input(" Enter a todo: ") + "\n"
 
-            with open('todos.txt', 'r') as file:
-                 todos = file.readlines()
+    if 'add' in user_action:
+        todo = user_action[4:]
+
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
+
+        todos.append(todo)
+
+        with open(r'C:\Users\seaer\PycharmProjects\pythonProject\todos.txt', 'w') as file:
+            file.writelines(todos)
+
+    if "show" in user_action:
+        with open(r'C:\Users\seaer\PycharmProjects\pythonProject\todos.txt', 'r') as file:
+            todos = file.readlines()
+
+        # list comprehension
+        new_todos = [item.strip('\n') for item in todos]
+        # list comprehension
+
+        for index, item in enumerate(new_todos):
+            row = f"{index + 1}-{item}"
+            print(row)
+
+    if 'edit' in user_action:
+        number = int(input("Number of the todo to edit: "))
+        number = number - 1
+
+        with open(r'C:\Users\seaer\PycharmProjects\pythonProject\todos.txt', 'r') as file:
+            todos = file.readlines()
+            print('here is todos existing files', todos)
+        new_todo = input("enter new todo: ")
+        todos[number] = new_todo + '\n'
+        with open(r'C:\Users\seaer\PycharmProjects\pythonProject\todos.txt', 'w') as file:
+            file.writelines(todos)
+
+    if 'complete' in user_action:
+
+        number = int(input("Number of the todo to complete: "))
+
+        with open(r'C:\Users\seaer\PycharmProjects\pythonProject\todos.txt', 'r') as file:
+            todos = file.readlines()
+
+        todos.pop(number - 1)
+
+        with open(r'C:\Users\seaer\PycharmProjects\pythonProject\todos.txt', 'w') as file:
+            file.writelines(todos)
 
 
-            todos.append(todo)
 
-            with open('todos.txt', 'w') as file:
-                 file.writelines(todos)
-
-        case 'show':
-            with open('todos.txt', 'r') as file:
-                 todos = file.readlines()
-
-
-            # list comprehension
-            new_todos = [item.strip('\n') for item in todos]
-            # list comprehension
-
-
-            for index, item in enumerate(new_todos):
-                row = f"{index + 1}-{item}"
-                print(row)
-        case 'edit':
-
-            number = int(input("Number of the todo to edit: "))
-            number = number -1
-            new_todo = input("enter new todo: ")
-            todos[number] = new_todo
-        case 'complete':
-              number = int(input("Number of the todo to complete: "))
-              todos.pop(number - 1)
-
-        case 'exit':
-            break
+    if 'exit' in user_action:
+        break
 
 print("bye!")
